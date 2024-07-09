39afbac7124bb659f8cd26b9bf21c99bc405cf17
kongukjae <51992661+kongukjae@users.noreply.github.com>
2024-07-09 21:57:22 +0900

    config : webpack module.rules property 설명

--- webpack.config.js
+++ webpack.config.js
@@ -6,6 +6,14 @@ module.exports = {
     filename: "app.bundle.js",
     path: path.resolve(__dirname, "dist")
   },
-  module:"",
+  module:{
+    rules:[
+      {
+        test:"",      // 정규식을 사용하여 파일을 찾는다.
+        use: "",      // 사용할 로더를 정의한다.
+        exclude:""    // 제외할 파일을 정의한다.
+      }
+    ]
+  },
   resolve:"",
 };
