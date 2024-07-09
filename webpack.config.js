e04fdb8c33682a61fac08f9657fc531b53414e45
kongukjae <51992661+kongukjae@users.noreply.github.com>
2024-07-09 21:58:14 +0900

    config : webpack module.rules ts-loader setup

--- webpack.config.js
+++ webpack.config.js
@@ -9,9 +9,9 @@ module.exports = {
   module:{
     rules:[
       {
-        test:"",      // 정규식을 사용하여 파일을 찾는다.
-        use: "",      // 사용할 로더를 정의한다.
-        exclude:""    // 제외할 파일을 정의한다.
+        test: /\.ts?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
       }
     ]
   },
