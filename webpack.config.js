a7f1fedd7283b7bac522cd4c6dfab2910e32967f
kongukjae <51992661+kongukjae@users.noreply.github.com>
2024-07-09 21:55:36 +0900

    config : webpack output

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,10 @@ const path = require('path');
 
 module.exports = {
   entry: "./src/app.ts",
-  output:"",
+  output:{
+    filename: "app.bundle.js",
+    path: path.resolve(__dirname, "dist")
+  },
   module:"",
   resolve:"",
 };
